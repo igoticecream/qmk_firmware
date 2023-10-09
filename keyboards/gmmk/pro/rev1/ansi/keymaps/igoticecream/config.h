9315d63a764f1dcf100e8a341e09fb1a9a9ed1ea
Pedro Díaz <pedro@omniarcs.com>
2023-10-09 18:22:57 +0200

    Add old layouts

---
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/igoticecream/config.h
@@ -0,0 +1,8 @@
+/*
+Config to suspend RGB when computer is off or sleeping
+See: https://beta.docs.qmk.fm/using-qmk/hardware-features/lighting/feature_rgb_matrix#suspended-state-id-suspended-state
+*/
+#define RGB_DISABLE_WHEN_USB_SUSPENDED true
+
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES  // or #define RGB_MATRIX_KEYRELEASES
