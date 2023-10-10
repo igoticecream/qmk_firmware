125dff04b2c90db73f5b96d73d0cbf6e9c8dadbd
Pedro Díaz <pedro@omniarcs.com>
2023-10-10 16:24:02 +0200

    Game layout and custom rgb matrix

--- keyboards/gmmk/pro/rev1/ansi/keymaps/igoticecream/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/igoticecream/keymap.c
@@ -16,6 +16,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+enum layers {
+    BASE = 0,
+    GAME,
+    TEMP,
+};
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -38,20 +44,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // This keyboard defaults to 6KRO instead of NKRO for compatibility reasons (some KVMs and BIOSes are incompatible with NKRO).
     // Since this is, among other things, a "gaming" keyboard, a key combination to enable NKRO on the fly is provided for convenience.
     // Press Fn+N to toggle between 6KRO and NKRO. This setting is persisted to the EEPROM and thus persists between restarts.
-    [0] = LAYOUT(
+    [BASE] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MPLY,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(2),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
-    [1] = LAYOUT(
+    [GAME] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        KC_LCTL, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
+        XXXXXXX, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
+    ),
+
+    [TEMP] = LAYOUT(
         QK_BOOT, KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, KC_SCRL, KC_PAUS,          KC_MUTE,
         _______, KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,   KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_PMNS, KC_PPLS, _______,          KC_INS,
         _______, _______, RGB_VAI, RGB_HUI, _______, _______, _______, _______, _______, _______, _______, KC_PSLS, KC_PAST, _______,          _______,
-        _______, _______, RGB_VAD, RGB_HUD, _______, _______, _______, _______, _______, _______, _______, _______,          KC_PENT,          _______,
+        TG(1),   _______, RGB_VAD, RGB_HUD, _______, _______, _______, _______, _______, _______, _______, _______,          KC_PENT,          _______,
         _______,          _______, _______, _______, _______, _______, NK_TOGG, _______, _______, KC_PDOT, _______,          _______, RGB_MOD, KC_HOME,
         _______, _______, _______,                            RGB_TOG,                            _______, _______, _______, RGB_SPD, RGB_RMOD, RGB_SPI
     ),
@@ -61,8 +76,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
+    [BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [GAME] = { ENCODER_CCW_CW(_______, _______) },
+    [TEMP] = { ENCODER_CCW_CW(_______, _______) }
 };
 #endif
 // clang-format on
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    switch (get_highest_layer(layer_state)) {
+        case BASE:
+            rgb_matrix_set_color_all(STATUS_LAYER_BASE_COLOR);
+            break;
+        case GAME:
+            rgb_matrix_set_color_all(STATUS_LAYER_GAME_COLOR);
+            break;
+        case TEMP:
+            rgb_matrix_set_color_all(STATUS_LAYER_TEMP_COLOR);
+            break;
+    }
+
+    if (rgb_matrix_get_mode() == RGB_MATRIX_STATUS) {
+        if (host_keyboard_led_state().caps_lock) {
+            for (uint8_t i = 0; i < RGB_MATRIX_LED_COUNT; ++i) {
+                if (HAS_ANY_FLAGS(g_led_config.flags[i], LED_FLAG_UNDERGLOW)) {
+                    rgb_matrix_set_color(i, STATUS_CAPS_COLOR);
+                }
+            }
+        }
+    }
+
+    return false;
+}
